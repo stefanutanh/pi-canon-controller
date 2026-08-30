df95e12e1e39a3d67a9753d53cdffca4fa5ec065
stefanutanh <bergs83@gmail.com>
2026-08-30 19:04:31 +0200

    update

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-imports
+from fastapi.responses import FileResponse
+
+import gphoto2 as gp
+
+camera = None
+
 
 def get_camera():
+    global camera
+    if camera is None:
+        try:
+            camera = gp.Camera()
+            camera.init()
+        except Exception:
+            camera = None
+    return camera
 
-def get_settings
+
+def get_settings():
+    return get_camera().get_settings()
+
 
 def set_viewfinder():
+    get_camera().set_viewfinder(True)
+
 
 def enable_viewfinder():
+    get_camera().enable_viewfinder()
+
 
 def disable_viewfinder():
+    get_camera().disable_viewfinder()
+
+@app.get("/")
+def read_root():
+    return FileResponse("static/index.html")
+
